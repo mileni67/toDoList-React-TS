e85403c8c29c72061af6cfb54dca6b039ecbb93f
Oleksandr Vykhor <sashavihor67@gmail.com>
2025-10-21 21:00:03 +0200

    add layouts + helmet (info html swap)

---
+++ src/layouts/Layouts.tsx
@@ -0,0 +1,23 @@
+import { Outlet } from "react-router-dom"
+import { Header } from "../components/Header/Header"
+import { Helmet, HelmetProvider } from "react-helmet-async"
+
+export const Layout = () => {
+    return (
+        <HelmetProvider>
+            <Helmet>
+                <link rel="preconnect" href="https://fonts.googleapis.com" />
+                <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
+                <link
+                    href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;700&display=swap"
+                    rel="stylesheet"
+                />
+                <title>ToDo List — React App</title>
+                <meta name="description" content="Список заданий на React + TypeScript" />
+            </Helmet>
+
+            <Header />
+            <Outlet />
+        </HelmetProvider>
+    );
+};
