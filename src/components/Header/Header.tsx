2f46d0e0f518e57509fcf4d202c8d865118e3cea
Oleksandr Vykhor <sashavihor67@gmail.com>
2025-10-26 08:54:39 +0100

    added button

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,7 +1,10 @@
 import { NavLink } from 'react-router-dom'
 import classes from './Header.module.scss'
+import { useDispatch } from 'react-redux'
+import { toggleThemeAction } from '../../features/themeList'
 
 export const Header = () => {
+    const dispatch = useDispatch()
     const getActiveClass = ({ isActive }: { isActive: boolean }): string => {
         return isActive ? `${classes.active} ${classes.link}` : classes.link
     }
@@ -10,7 +13,11 @@ export const Header = () => {
             <div className={classes.container}>
                 <NavLink to="/" className={getActiveClass}>ToDo</NavLink>
                 <NavLink to="/list" className={getActiveClass}>List</NavLink>
+
+                <div className={classes.toggleButton}>
+                    <button onClick={() => dispatch(toggleThemeAction())}>toggle</button>
+                </div>
             </div>
-        </header>
+        </header >
     )
 }
